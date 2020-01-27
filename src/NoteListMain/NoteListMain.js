ed025a1b6553f3cd02244bf9b2808029188b3eb3
Gerardo Gutierrez <51174733+ggutierrez113@users.noreply.github.com>
2020-01-27 16:58:48 -0600

    Update NoteListMain.js

--- src/NoteListMain/NoteListMain.js
+++ src/NoteListMain/NoteListMain.js
@@ -1,33 +1,41 @@
-import React from 'react'
+import React, {Component} from 'react'
 import { Link } from 'react-router-dom'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import Note from '../Note/Note'
 import CircleButton from '../CircleButton/CircleButton'
-import ApiContext from '../ApiContext'
-import { getNotesForFolder } from '../notes-helpers'
 import './NoteListMain.css'
+import NotesContext from '../NotesContext';
 
-export default class NoteListMain extends React.Component {
-  static defaultProps = {
-    match: {
-      params: {}
-    }
-  }
-  static contextType = ApiContext
+class NoteListMain extends Component {
 
-  render() {
-    const { folderId } = this.props.match.params
-    const { notes=[] } = this.context
-    const notesForFolder = getNotesForFolder(notes, folderId)
+  static contextType = NotesContext
+  render(){
+    const {notes} = this.context;
+    const folderId = this.props.match.params.folderId
+
+    const notesInFolder = notes.filter((note) => 
+    {if(folderId){
+     return  note.folderId === folderId
+    } else{
+      return note
+    }}
+  );
+
+
     return (
+
+      !this.props.err ?
+
       <section className='NoteListMain'>
         <ul>
-          {notesForFolder.map(note =>
+          {notesInFolder.map(note =>
             <li key={note.id}>
               <Note
                 id={note.id}
                 name={note.name}
                 modified={note.modified}
+                history={this.props.history}
+                match={this.props.match}
               />
             </li>
           )}
@@ -45,6 +53,12 @@ export default class NoteListMain extends React.Component {
           </CircleButton>
         </div>
       </section>
+
+      :
+      
+      <h3>{this.props.error}</h3>
     )
   }
 }
+
+export default NoteListMain;
